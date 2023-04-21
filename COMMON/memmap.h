80d4e9d4e0eacef8a542400678fea047719b1565
msayed6789 <128575094+msayed6789@users.noreply.github.com>
2023-04-21 15:06:04 +0200

    Add files via upload

---
+++ COMMON/memmap.h
@@ -0,0 +1,39 @@
+
+
+#ifndef MEMMAP_H_
+#define MEMMAP_H_
+
+
+/*************************DIO*********************/
+#define DDRA  (*(volatile unsigned char *)0x3A)
+#define PORTA (*(volatile unsigned char *)0x3B)
+#define PINA  (*(volatile unsigned char *)0x39)
+
+
+#define DDRB  (*(volatile unsigned char *)0x37)
+#define PORTB (*(volatile unsigned char *)0x38)
+#define PINB  (*(volatile unsigned char *)0x36)
+
+
+#define DDRD  (*(volatile unsigned char *)0x31)
+#define PORTD (*(volatile unsigned char *)0x32)
+#define PIND  (*(volatile unsigned char *)0x30)
+
+
+#define DDRC  (*(volatile unsigned char *)0x34)
+#define PORTC (*(volatile unsigned char *)0x35)
+#define PINC  (*(volatile unsigned char *)0x33)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* MEMMAP_H_ */
