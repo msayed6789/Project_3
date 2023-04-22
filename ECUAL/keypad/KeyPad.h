a96372123477dc99e56d83033cb5bb9ec2d6aada
msayed6789 <128575094+msayed6789@users.noreply.github.com>
2023-04-22 14:48:26 +0200

    Update KeyPad.h

--- ECUAL/keypad/KeyPad.h
+++ ECUAL/keypad/KeyPad.h
@@ -10,12 +10,9 @@
 #define KEYPAD_H_
 
 #include "DIO_Interface.h"
-//#include "Timers.h"
+
 #include "time_out.h"
 /*************************config**********************/
-/*
-#define FIRST_OUTPUt PINB0
-#define FIRST_INPUT  PINC0
 
 #define COL   4
 #define ROW   4
@@ -24,22 +21,8 @@
 
 static u8 KeysArray[ROW][COL]={{'7','8','9','/'},
                                {'4','5','6','*'},
-						       {'1','2','3','-'},
-						       {'c','0','=','+'}};		
-							
-/*******************************************************/						
-
-//u8 KEYPAD_GetKey(void);
-
-#define COL   4
-#define ROW   4
-
-#define  NO_KEY  'N'
-
-static u8 KeysArray[ROW][COL]={{'7','8','9','/'},
-{'4','5','6','*'},
-{'1','2','3','-'},
-{'c','0','=','+'}};
+                               {'1','2','3','-'},
+                               {'c','0','=','+'}};
 
 /*******************************************************/
 typedef enum{
