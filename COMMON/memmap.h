0a19d09030c1d078cdd1ce96c994790f787998ce
khalifaaa444 <116544385+khalifaaa444@users.noreply.github.com>
2023-04-23 10:37:57 +0200

    Update memmap.h

--- COMMON/memmap.h
+++ COMMON/memmap.h
@@ -27,6 +27,14 @@
 
 
 
+/****************************ADC******************************/
+
+
+#define ADMUX	(*(volatile unsigned char *)0x27)
+#define ADCSRA  (*(volatile unsigned char *)0x26)
+#define ADCH	(*(volatile unsigned char *)0x25)
+#define ADCL	(*(volatile unsigned char *)0x24)
+
 
 
 
