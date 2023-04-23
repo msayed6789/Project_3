02cc6922376e07466874c884d3214c31b2e9d0b1
khalifaaa444 <116544385+khalifaaa444@users.noreply.github.com>
2023-04-23 16:23:22 +0200

    Update memmap.h

--- COMMON/memmap.h
+++ COMMON/memmap.h
@@ -32,8 +32,8 @@
 
 #define ADMUX	(*(volatile unsigned char *)0x27)
 #define ADCSRA  (*(volatile unsigned char *)0x26)
-#define ADCH	(*(volatile unsigned char *)0x25)
-#define ADCL	(*(volatile unsigned char *)0x24)
+#define ADCH	(*(volatile unsigned int *)0x25)
+#define ADCL	(*(volatile unsigned int *)0x24)
 
 
 
