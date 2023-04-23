00075f68fc20496e43e44a720aec50986478b8c0
khalifaaa444 <116544385+khalifaaa444@users.noreply.github.com>
2023-04-23 10:50:04 +0200

    Create bazzer.h

---
+++ ECUAL/bazzer/bazzer.h
@@ -0,0 +1,23 @@
+/*
+ * bazzer.h
+ *
+ * Created: 4/20/2023 11:13:24 AM
+ *  Author: ebrahem
+ */ 
+
+
+#ifndef BAZZER_H_
+#define BAZZER_H_
+
+#include "dio.h"
+
+
+
+void bazz_init(PIn_name pin_num);
+void bazz_ON(PIn_name pin_num);
+void bazz_OFF(PIn_name pin_num);
+
+
+
+
+#endif /* BAZZER_H_ */
