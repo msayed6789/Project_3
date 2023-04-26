5074a431dc7e6dc536fb62c9bab0303838c40108
msayed6789 <128575094+msayed6789@users.noreply.github.com>
2023-04-27 00:00:18 +0200

    Update app.h

--- APPLICATION/app.h
+++ APPLICATION/app.h
@@ -13,6 +13,16 @@
 #include "sensor.h"
 #include "buzzer.h"
 #include "delay.h"
+/**************************************************************************************************************/
+#define MAX_TEMP      35       //Temperature in Celsius
+#define MIN_TEMP      18      //Temperature in Celsius
+#define DEFAULT_TEMP  20      //Temperature in Celsius
+/******************************************************Wire*****************************************************/
+#define BUZZER_LED    PIND6
+#define KEYPAD_ROW    PINB0
+#define KEYPAD_COL    PINB4
+#define TEMP_SENSOR   PINA0
+/***************************************************************************************************************/
 void App_init(void);
 void App_start(void);
 
