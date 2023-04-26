5932f825f4fafaf5db6d4a87a34164259940d526
msayed6789 <128575094+msayed6789@users.noreply.github.com>
2023-04-26 23:25:14 +0200

    Update delay.c

--- SERVICES/delay/delay.c
+++ SERVICES/delay/delay.c
@@ -27,17 +27,3 @@ void Delay_ms(u32 milliseconds)
 	TIMER2_Stop ();
 }
 
-void delay_us(u32 microseconds)
-{
-	TIMER2_init (normal,ticks_250);
-	TIMER2_start (pres_8);
-	u32 i;
-	for(i=0;i< microseconds ;i++){
-		// N = (1000us)/(250u);  //= 4 without approximation
-		//for one millisecond delay iterate 4 times of timer2 overflow
-		while((TIFR & clear_T2) == 0);
-		TIMER2_set(ticks_100);
-		TIFR |= clear_T2;
-	}
-	TIMER2_Stop ();
-}
