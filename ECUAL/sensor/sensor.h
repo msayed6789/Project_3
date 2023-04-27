03411c5bf0ed538d645f1904b05b1a9143bd4813
msayed6789 <128575094+msayed6789@users.noreply.github.com>
2023-04-27 17:25:54 +0200

    Update sensor.h

--- ECUAL/sensor/sensor.h
+++ ECUAL/sensor/sensor.h
@@ -3,17 +3,18 @@
  *
  * Created: 4/20/2023 11:17:16 AM
  *  Author: ebrahem
- */ 
+ */
 
 
 #ifndef SENSOR_H_
 #define SENSOR_H_
 
 #include "adc.h"
+#include "dio.h"
 
 void Temp_init(PIn_name channel);
 
-uint8_t Temp_Read(PIn_name channel);
+void Temp_Read(PIn_name channel,f32* temp);
 
 
 
